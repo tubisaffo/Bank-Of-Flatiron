da7fe467a2407bd3294446baac924b780161d8cd
tubisaffo <tubisaffo@gmail.com>
2024-04-29 05:11:50 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import "./App.css";
+
+import SearchForm from "./components/SearchForm";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title">
+        <h2>Bank of Flatiron</h2>
+      </div>
+      <SearchForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchForm.js
@@ -0,0 +1,10 @@
+// import React from "react";
+
+export default function SearchForm() {
+  return (
+    <form id="search-form">
+      <input type="text" id="search-input" />
+      <button>Search</button>
+    </form>
+  );
+}
